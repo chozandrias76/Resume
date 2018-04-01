f4f9523c980d948221b28f6f269a1ff6d5cf7532
Colin Swenson-Healey <chozandrias76@gmail.com>
2018-04-01 14:06:58 -0700

    Update Blog.jsx

--- client/components/Blog.jsx
+++ client/components/Blog.jsx
@@ -6,7 +6,7 @@ class BlogComponent extends Component {
     render() {
     return (
       <div>
-        <h1>Coming soon!&#8482;</h2>
+        <h1>Coming soon!&#8482;</h1>
         <iframe src="http://localhost:8000/"  style={{ width: '100%', height: '100%' }}/>
       </div>
     );
